b47974d6e1ff2d67f67311439770f6297f01e8bd
KingOffling <125208496+KingOffling@users.noreply.github.com>
2023-10-15 23:52:51 -0400

    okok

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 import WagdieCreator from './components/WagdieCreator';
 import MobileView from './components/MobileView';
 
 function App() {
-    const isMobile = window.innerWidth <= 768; // This will return true for screen widths up to and including 768 pixels.
+    // Initial check for mobile display
+    const [isMobile, setIsMobile] = useState(window.innerWidth <= 768 || window.innerHeight <= 635);
+
+    useEffect(() => {
+        // Handler to set isMobile based on window width
+        const handleResize = () => {
+            setIsMobile(window.innerWidth <= 768 || window.innerHeight <= 635);
+        };
+
+        // Add event listener
+        window.addEventListener('resize', handleResize);
+
+        // Cleanup listener on component unmount
+        return () => {
+            window.removeEventListener('resize', handleResize);
+        };
+    }, []);  // Empty dependency array ensures this effect runs only once on mount and cleanup on unmount
 
     return (
         <div className="App">
